294fe1eb24fb780c4d837ee9a4c75e7d8f2d7cd6
Emerson Huaman <emerson.huaman1@unmsm.edu.pe>
2025-01-11 20:22:35 -0500

    Modelos

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -101,4 +101,50 @@
                 return true;    //si tiene errores
             }
         }
+
+        //Función paginador de tablas
+        protected static function paginador_tablas($pagina, $Npaginas, $url, $botones){
+            $tabla = '<nav aria-label="Page navigation example"><ul class="pagination 
+                justify-content-center">';
+            
+            if($pagina==1){
+                $tabla.= '<li class="page-item disabled"><a class="page-link">
+                    <i class="fas fa-angle-double-left"></i></a></li>';
+            }else{
+                $tabla.= 
+                '<li class="page-item"><a class="page-link" href='.$url.'1/">
+                    <i class="fas fa-angle-double-left"></i></a></li>
+                <li class="page-item"><a class="page-link href="'.$url.($pagina-1).
+                    '/">Anterior</a></li>';
+            }
+
+            $ci=0;
+            for($i=$pagina; $i<=$Npaginas; $i++){
+                if($ci>=$botones){
+                    break;
+                }
+                if($pagina==$i){
+                    $tabla.= '<li class="page-item"><a class="page-link active" href='
+                        .$url.$i.'/">'.$i.'</a></li>';
+                }else{
+                    $tabla.= '<li class="page-item"><a class="page-link" href='
+                        .$url.$i.'/">'.$i.'</a></li>';
+                }
+                $ci++;
+            }
+
+            if($pagina==$Npaginas){
+                $tabla.= '<li class="page-item disabled"><a class="page-link">
+                    <i class="fas fa-angle-double-right"></i></a></li>';
+            }else{
+                $tabla.= 
+                '<li class="page-item"><a class="page-link" href='.$url.($pagina+1).
+                    '/">Siguiente</a></li>
+                <li class="page-item"><a class="page-link" href='.$url.$Npaginas.'">
+                    <i class="fas fa-angle-double-left"></i></a></li>';
+            }
+            
+            $tabla.= '</url></nav>'; //concatenación
+            return $tabla;
+        }
     }
